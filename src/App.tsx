f6ead9cca89240fe880748520e45a584997c11a0
Jakub Pawlowski <jakub.pawlowski00@gmail.com>
2023-05-15 02:40:48 +0200

    Create validation skeleton, set up types.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
 import React, {useState} from 'react';
+import {SubmitHandler, useForm} from "react-hook-form";
 import './App.css';
 
-function App() {
+interface IDish {
+    name: String;
+    options?: String[];
+}
 
-  return <div className="App">
-      <div className="flex h-screen justify-center items-center">
-          <div className="mockup-window bg-base-300 shadow-2xl">
-              <div className="grid grid-cols-1 gap-4 px-4 py-4 bg-base-200">
-                  <h2 className="text-center">Input your favorite dish!</h2>
-                  <input placeholder="Dish Name" className="text-center input input-bordered"></input>
-                  <input placeholder="Preparation (Duration)" className="text-center input input-bordered"></input>
-                  <select className="select select-bordered">
-                      <option value="" disabled selected hidden>Type:</option>
-                      <option value="pizza">Pizza</option>
-                      <option value="soup">Soup</option>
-                      <option value="sandwich">Sandwich</option>
-                  </select>
-              </div>
-          </div>
-      </div>
-  </div>;
+interface IFormInput {
+    dishName: String;
+    dishDuration: String; //TODO zmienic na czas
+}
+
+function App() {
+    const [dishType, setDishType] = useState("");
+    const [showOption, setShowOption] = useState(false);
+
+    const { register, handleSubmit } = useForm<IFormInput>();
+    const onSubmit: SubmitHandler<IFormInput> = data => console.log(data);
+
+    const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setDishType(e.target.value);
+        if (e.target.value !== "") setShowOption(true);
+        else setShowOption(false);
+    };
+
+    return <div className="App">
+        <div className="flex h-screen justify-center items-center">
+            <div className="mockup-window bg-base-300 shadow-2xl">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="grid grid-cols-1 gap-4 px-4 py-4 bg-base-200">
+                        <h2 className="text-center">Input your favorite dish!</h2>
+                        <input {...register("dishName", { required: true })} placeholder="Dish Name"
+                               className="text-center input input-bordered"></input>
+                        <input {...register("dishDuration", { required: true })} placeholder="Preparation (Duration)"
+                               className="text-center input input-bordered"></input>
+                        <select onChange={handleSelectChange} className="select select-bordered">
+                            <option value="" disabled selected hidden>Type:</option>
+                            <option value="pizza">Pizza</option>
+                            <option value="soup">Soup</option>
+                            <option value="sandwich">Sandwich</option>
+                        </select>
+                        {
+                            showOption ? <p>test!</p> : ""
+                        }
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>;
 }
 
 export default App;
