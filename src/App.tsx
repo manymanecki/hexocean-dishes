a6eeb0a3a4a163f3cddae7a7d162e1119cbb49e7
Jakub Pawlowski <jakub.pawlowski00@gmail.com>
2023-05-15 18:02:15 +0200

    Update to conditional rendering, using fieldArray

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,56 @@
-import React, {useState} from 'react';
-import {SubmitHandler, useForm} from "react-hook-form";
-import DishesList from './DishesList.json';
+import React, { useState } from 'react';
+import { SubmitHandler, useForm, useFieldArray } from "react-hook-form";
+import DishConfig from './DishConfig.json';
 import './App.css';
 
-const dishes: IFormInput[] = DishesList;
+const dishes: Configuration[] = DishConfig;
 
-interface IFormInput {
-    name?: String;
-    preparation_time?: String; //TODO zmienic na czas
-    type: String;
-    options?: String[];
+export interface IFormInput {
+    name: string
+    preparation_time: string
+    configuration: Configuration
 }
+
+export interface Configuration {
+    type: string
+    options: Option[]
+}
+
+export interface Option {
+    name: string
+    type: string
+}
+
 
 function App() {
     const [ dishType, setDishType ] = useState("");
-    const { register, handleSubmit } = useForm<IFormInput>();
+    const { register, handleSubmit, control } = useForm<IFormInput>();
+    const { fields, append, remove } = useFieldArray({
+        control,
+        name: "configuration.options"
+    });
 
     const onSubmit: SubmitHandler<IFormInput> = data => console.log(data);
-
-    let fetchAPI = fetch('https://umzzcc503l.execute-api.us-west-2.amazonaws.com/dishes/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            'name': 'HexOcean pizza',
-            'preparation_time': '01:30:22',
-            'type': 'pizza',
-            'no_of_slices': 4,
-            'diameter': 33.4
-        })
-    });
+    // let fetchAPI = fetch('https://umzzcc503l.execute-api.us-west-2.amazonaws.com/dishes/', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({
+    //         'name': 'HexOcean pizza',
+    //         'preparation_time': '01:30:22',
+    //         'type': 'pizza',
+    //         'no_of_slices': 4,
+    //         'diameter': 33.4
+    //     })
+    // });
 
     const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         setDishType(e.target.value);
+        console.log(fields)
     };
 
-    const dishOptions = dishes.find((dish) => dish.type === dishType)?.options || [];
+    // const configurationOptions = dishes.find((dish) => dish.type === dishType) || [];
 
     return <div className="App">
         <div className="flex h-screen justify-center items-center">
@@ -48,7 +62,7 @@ function App() {
                         <input {...register("preparation_time", { required: true })} type="text" placeholder="hh:mm:ss"
                                className="text-center input input-bordered">
                         </input>
-                        <select {...register("type", { required: true })} value={dishType} onChange={handleSelectChange}
+                        <select {...register("configuration.type", { required: true })} value={dishType} onChange={handleSelectChange}
                                 className="select select-bordered">
                             <option value="" disabled hidden>Dish type:</option>
                             {dishes.map((dish) => (
@@ -57,22 +71,32 @@ function App() {
                                 </option>
                             ))}
                         </select>
-                        {dishOptions.length > 0 && (
-                            <>
-                                {dishOptions.map((option) => (
-                                    <div key={option.toString()}>
-                                        <input
-                                            type="number"
-                                            step="any"
-                                            className="text-center input input-bordered"
-                                            id={option.toString()}
-                                            name={option.toString()}
-                                            placeholder={option.replace(/_/g, " ")}>
-                                        </input>
-                                    </div>
-                                ))}
-                            </>
-                        )}
+                        {fields.map((field, index) => (
+                            <div key={field.id}>
+                                <input
+                                    {...register(`configuration.options.${index}.name` as const, { required: true })}
+                                    type={`configuration.options.${index}.type`}
+                                    placeholder={`configuration.options.${index}.name`}
+                                    className="text-center input input-bordered"
+                                />
+                            </div>
+                        ))}
+                        {/*{dishOptions.length > 0 && (*/}
+                        {/*    <>*/}
+                        {/*        {dishOptions.map((option) => (*/}
+                        {/*            <div key={option.toString()}>*/}
+                        {/*                <input*/}
+                        {/*                    type="number"*/}
+                        {/*                    step="any"*/}
+                        {/*                    className="text-center input input-bordered"*/}
+                        {/*                    id={option.toString()}*/}
+                        {/*                    name={option.toString()}*/}
+                        {/*                    placeholder={option.replace(/_/g, " ")}>*/}
+                        {/*                </input>*/}
+                        {/*            </div>*/}
+                        {/*        ))}*/}
+                        {/*    </>*/}
+                        {/*)}*/}
                         <button className="btn" disabled={false}>Submit</button>
                     </div>
                 </form>
