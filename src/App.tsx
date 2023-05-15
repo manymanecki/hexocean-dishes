35f18a1486200b566ea6c77fcb824af683353e9c
Jakub Pawlowski <jakub.pawlowski00@gmail.com>
2023-05-15 04:04:39 +0200

    Create conditional rendering for options

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import DishJSON from './Dish.json';
 import './App.css';
 
 interface IDish {
-    name: String;
+    type: String;
     options?: String[];
 }
 
@@ -17,38 +17,45 @@ interface IFormInput {
 
 function App() {
     const [dishType, setDishType] = useState("");
-    const [showOption, setShowOption] = useState(false);
 
     const { register, handleSubmit } = useForm<IFormInput>();
     const onSubmit: SubmitHandler<IFormInput> = data => console.log(data);
 
     const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         setDishType(e.target.value);
-        if (e.target.value !== "") setShowOption(true);
-        else setShowOption(false);
     };
+
+    const dishOptions = dishes.find((dish) => dish.type === dishType)?.options || [];
 
     return <div className="App">
         <div className="flex h-screen justify-center items-center">
             <div className="mockup-window bg-base-300 shadow-2xl">
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="grid grid-cols-1 gap-4 px-4 py-4 bg-base-200">
-                        <h2 className="text-center">Input your favorite dish!</h2>
                         <input {...register("dishName", { required: true })} placeholder="Dish Name"
                                className="text-center input input-bordered"></input>
-                        <input {...register("dishDuration", { required: true })} placeholder="Preparation (Duration)"
-                               className="text-center input input-bordered"></input>
+                        <input type="text" name="duration" id="durationForm" maxLength={8}
+                               pattern="^((\d+:)?\d+:)?\d*$" placeholder="hh:mm:ss" className="text-center input input-bordered">
+                        </input>
                         <select value={dishType} onChange={handleSelectChange} className="select select-bordered">
                             <option value="" disabled selected hidden>Dish type?</option>
                             {dishes.map((dish) => (
-                                <option key={dish.name.toString()} value={dish.name.toString()}>
-                                    {dish.name.toString()}
+                                <option key={dish.type.toString()} value={dish.type.toString()}>
+                                    {dish.type.toString()}
                                 </option>
                             ))}
                         </select>
-                        {
-                            showOption ? <p>test!</p> : ""
-                        }
+                        {dishOptions.length > 0 && (
+                            <>
+                                {dishOptions.map((option) => (
+                                    <div key={option.toString()}>
+                                        <input type="text" className="text-center input input-bordered"
+                                               id={option.toString()} name={option.toString()} placeholder={option.toString()} />
+                                    </div>
+                                ))}
+                            </>
+                        )}
+                        <button className="btn" disabled={false}>Submit</button>
                     </div>
                 </form>
             </div>
